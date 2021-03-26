0bcf97c39cfc6124739ca294f0f0f1e1181a50c4
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-03-26 09:49:28 -0300

    table Users

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,12 +2,24 @@
   <section class="section">
     <h1 class="is-size-3">Selecione um Usuário</h1>
     <section class="section">
-      <b-table
-        :data="listUser"
-        :columns="tableColumns"
-        hoverable
-        @select="onUserSelect"
-      ></b-table>
+      <table v-if="!isLoading" class="table">
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Dívida</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="user in listUser" :key="user._id">
+            <td>{{ user.name }}</td>
+            <td>{{ user.username }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{ user.formattedDebtTotal }}</td>
+          </tr>
+        </tbody>
+      </table>
     </section>
     <b-loading v-model="isLoading" :is-full-page="true">
       <clip-loader
@@ -21,6 +33,8 @@
 
 <script>
 import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
+import Numeral from 'numeral'
+import 'numeral/locales/pt-br'
 
 export default {
   name: 'HomePage',
@@ -30,32 +44,38 @@ export default {
   data() {
     return {
       isLoading: true,
-      listUser: [],
-      tableColumns: [
-        {
-          field: 'name',
-          label: 'Nome',
-        },
-        {
-          field: 'username',
-          label: 'Username',
-        },
-        {
-          field: 'email',
-          label: 'Email',
-        },
-      ],
     }
   },
-  async mounted() {
-    const listUser = await this.$axios.$get('/api/users')
-    this.listUser = listUser
-    this.isLoading = false
+  computed: {
+    listUser() {
+      return this.$store.state.listUser
+    },
+  },
+  mounted() {
+    this.fetchListUser(this)
   },
   methods: {
     onUserSelect(user) {
       console.log('user:', user)
     },
+    fetchListUser: async (vm) => {
+      vm.isLoading = true
+      let listUser = await vm.$axios.$get('/api/users')
+      listUser = listUser.map((user, index) => {
+        Numeral.locale('pt-br')
+        user.formattedDebtTotal =
+          'R$ ' + Numeral(user.debtTotal).format('0,000.00')
+        return user
+      })
+      vm.$store.dispatch('setListUser', listUser)
+      vm.isLoading = false
+    },
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.cursor-pointer {
+  cursor: pointer;
+}
+</style>
