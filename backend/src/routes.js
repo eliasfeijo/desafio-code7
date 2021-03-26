fa0879bbc9739df5272bd9d40f344bfaa22d1fae
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-03-25 21:42:40 -0300

    refactoring

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const routes = express.Router();
 
-routes.get("/", function(req, res) {
+routes.get("/", (req, res) => {
   return res.send("Minha primeira rota!");
 });
 
