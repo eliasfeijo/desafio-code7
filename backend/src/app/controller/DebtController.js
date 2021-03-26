a41cff922d132f5cf61d61e4ae0ef2011dba7879
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-03-25 22:42:04 -0300

    edit and delete methods

--- backend/src/app/controller/DebtController.js
+++ backend/src/app/controller/DebtController.js
@@ -3,14 +3,39 @@ const DebtSchema = require("../schema/DebtSchema");
 
 class DebtController {
   async index(req, res) {
-    const data = await DebtSchema.find({});
-
-    return res.json(data);
+    const listDebt = await DebtSchema.find({});
+    return res.json(listDebt);
   }
   async create(req, res) {
-    const data = await DebtSchema.create(req.body);
+    const debt = await DebtSchema.create(req.body);
+    if(debt.id) {
+      return res.json(debt);
+    }
+    else {
+      return res.status(500).send('Could not create Debt');
+    }
 
-    return res.json(data);
+  }
+  async edit(req, res) {
+    const debt = await DebtSchema.findById(req.params.id);
+    if(debt) {
+      debt = new DebtSchema(debt);
+      debt.save();
+      return res.json(debt);
+    }
+    else {
+      return res.status(404).send('Debt not found');
+
+    }
+  }
+  async delete(req, res) {
+    const debt = await DebtSchema.findByIdAndDelete(req.params.id, (err) => {
+      if(err) {
+        console.log(err);
+        return res.sendStatus(500);
+      }
+      return res.sendStatus(200);
+    });
   }
 }
 
