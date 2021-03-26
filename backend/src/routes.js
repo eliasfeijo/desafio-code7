6371399406e0c6f26eb9e4b087ee220a94597ea9
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-03-25 22:43:11 -0300

    debts PUT and DELETE

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,5 +5,7 @@ const routes = express.Router();
 
 routes.get("/debts", DebtController.index);
 routes.post("/debts", DebtController.create);
+routes.put("/debts/:id", DebtController.edit);
+routes.delete("/debts/:id", DebtController.delete);
 
 module.exports = routes;
