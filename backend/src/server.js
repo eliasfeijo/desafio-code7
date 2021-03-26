8c2e30d9c66213e1feef4fbc568ef583f422f727
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-03-25 21:42:33 -0300

    bootstrapUsers (populate User document)

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const db = require("./database/config");
 const mongoose = require("mongoose");
+const _ = require("lodash")
+const fetch = require("node-fetch");
+const UserSchema = require("./app/model/UserSchema");
 
 class App {
   constructor() {
@@ -17,6 +20,7 @@ class App {
 
   database() {
     mongoose.connect(db.uri, { useNewUrlParser: true });
+    this.bootstrapUsers();
   }
 
   middlewares() {
@@ -25,6 +29,32 @@ class App {
 
   routes() {
     this.express.use(require("./routes"));
+  }
+
+  async bootstrapUsers() {
+    let hasUsers = false;
+    await UserSchema.find({}, async (err, users) => {
+      if(err) {
+        console.log(`Error fetching User document`, err);
+      }
+      if(users && users.length > 0) {
+        hasUsers = true;
+      }
+    });
+    if(!hasUsers) {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      console.log(response);
+      if(!response.ok) {
+        const message = `An error has occured: ${response.status}`;
+        throw new Error(message);
+      }
+      const listUser = await response.json();
+      console.log(listUser);
+      _.forEach(listUser, user => {
+        const newUser = new UserSchema(user);
+        newUser.save();
+      });
+    }
   }
 }
 module.exports = new App().express;
