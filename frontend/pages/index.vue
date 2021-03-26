064974f90f3cc1eac52dd2bf160413ed77690216
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-03-26 01:27:24 -0300

    loading component

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,11 +1,28 @@
 <template>
   <section class="section">
     <div>Frontend</div>
+    <b-loading v-model="isLoading" :is-full-page="true">
+      <clip-loader
+        :loading="isLoading"
+        color="#6497b1"
+        size="60px"
+      ></clip-loader>
+    </b-loading>
   </section>
 </template>
 
 <script>
+import ClipLoader from 'vue-spinner/src/ClipLoader.vue'
+
 export default {
   name: 'HomePage',
+  components: {
+    ClipLoader,
+  },
+  data() {
+    return {
+      isLoading: true,
+    }
+  },
 }
 </script>
