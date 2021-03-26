9dc3e16829195010ca24e11f41ddb94dd576d6d7
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-03-26 09:34:11 -0300

    including listDebt and debtTotal on GET /users

--- backend/src/app/controller/UserController.js
+++ backend/src/app/controller/UserController.js
@@ -1,10 +1,23 @@
 
+const DebtSchema = require("../schema/DebtSchema");
 const UserSchema = require("../schema/UserSchema");
-
+const _ = require('lodash');
 class UserController {
   async index(req, res) {
     const listUser = await UserSchema.find({});
-    return res.json(listUser);
+    const jsonListUser = JSON.parse(JSON.stringify(listUser))
+    await Promise.all(
+      jsonListUser.map(async (user, index) => {
+        const listDebt = await DebtSchema.find({ user: user._id });
+        user.listDebt = listDebt;
+        let debtTotal = 0;
+        listDebt.map((debt, index) => {
+          debtTotal += debt.value;
+        });
+        user.debtTotal = debtTotal;
+      })
+    );
+    return res.end(JSON.stringify(jsonListUser));
   }
   async find(req, res) {
     const user = await UserSchema.findById(req.params.id);
