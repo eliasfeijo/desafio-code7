0c6b4ee07bcc871cf6f65efb3acf8bf1ef006407
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-03-26 13:58:05 -0300

    ModalForm for Debt

---
+++ frontend/components/screens/ModalForm.vue
@@ -0,0 +1,79 @@
+<template>
+  <form action="">
+    <div class="modal-card" style="width: auto">
+      <header class="modal-card-head">
+        <p class="modal-card-title">{{ labelTitle }}</p>
+        <button type="button" class="delete" @click="$emit('close')" />
+      </header>
+      <section class="modal-card-body">
+        <b-field label="Valor">
+          <b-input
+            type="number"
+            :value="value"
+            placeholder="Valor da Dívida"
+            required
+          >
+          </b-input>
+        </b-field>
+
+        <b-field label="Razão">
+          <b-input
+            type="text"
+            :value="reason"
+            placeholder="Razão da Dívida"
+            required
+          >
+          </b-input>
+        </b-field>
+
+        <b-field label="Data">
+          <b-input
+            type="text"
+            :value="dueDate"
+            placeholder="Data de vencimento"
+            required
+          >
+          </b-input>
+        </b-field>
+      </section>
+      <footer class="modal-card-foot">
+        <b-button label="Fechar" @click="$emit('close')" />
+        <b-button label="Cadastrar" type="is-primary" />
+      </footer>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'ModalForm',
+  props: {
+    isEdit: {
+      type: Boolean,
+      default: false,
+    },
+    debt: {
+      type: Object,
+      default: () => {
+        return {
+          reason: '',
+          dueDate: '',
+          value: '',
+        }
+      },
+    },
+  },
+  data() {
+    return {
+      reason: this.debt.reason,
+      dueDate: this.debt.dueDate,
+      value: this.debt.value,
+    }
+  },
+  computed: {
+    labelTitle() {
+      return this.isEdit ? 'Editar Dívida' : 'Cadastrar Dívida'
+    },
+  },
+}
+</script>
--- frontend/components/screens/UserScreen.vue
+++ frontend/components/screens/UserScreen.vue
@@ -10,11 +10,14 @@
       <p class="is-size-5">
         Dívida Total: {{ selectedUser.formattedDebtTotal }}
       </p>
-      <b-button type="is-primary" class="mt-4">Cadastrar Dívida</b-button>
+      <b-button type="is-primary" class="mt-4" @click="isModalActive = true"
+        >Cadastrar Dívida</b-button
+      >
     </div>
     <div class="columns">
       <div class="column is-half is-offset-one-quarter">
         <b-table
+          v-if="selectedUser.listDebt.length > 0"
           class="cursor-pointer"
           :data="selectedUser.listDebt"
           :columns="tableColumns"
@@ -22,12 +25,30 @@
         ></b-table>
       </div>
     </div>
+    <b-modal
+      v-model="isModalActive"
+      has-modal-card
+      trap-focus
+      :destroy-on-hide="false"
+      aria-role="dialog"
+      aria-label="Modal Dívida"
+      aria-modal
+    >
+      <template #default="props">
+        <modal-form :is-edit="isEditModal" @close="props.close"></modal-form>
+      </template>
+    </b-modal>
   </div>
 </template>
 
 <script>
+import ModalForm from './ModalForm'
+
 export default {
   name: 'UserScreen',
+  components: {
+    ModalForm,
+  },
   data() {
     return {
       tableColumns: [
@@ -44,6 +65,8 @@ export default {
           label: 'Valor',
         },
       ],
+      isModalActive: false,
+      isEditModal: false,
     }
   },
   computed: {
