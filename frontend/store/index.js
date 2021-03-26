12edb5f8862e1fd15af1f5d0c5dde8ed18380b01
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-03-26 10:20:17 -0300

    adding currentScreen state

--- frontend/store/index.js
+++ frontend/store/index.js
@@ -1,14 +1,21 @@
 export const state = () => ({
+  currentScreen: 'HOME',
   listUser: [],
 })
 
 export const mutations = {
+  setCurrentScreen(state, screen) {
+    state.currentScreen = screen
+  },
   setListUser(state, listUser) {
     state.listUser = listUser
   },
 }
 
 export const actions = {
+  setCurrentScreen(context, screen) {
+    context.commit('setCurrentScreen', screen)
+  },
   setListUser(context, listUser) {
     context.commit('setListUser', listUser)
   },
